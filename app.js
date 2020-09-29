6be9ee16067973ca62a56fc0f4ec8d73ab041c9a
LanceRabbit <lancerabbit@gmail.com>
2020-09-29 09:53:26 +0800

    google clinet login

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+'use strict';
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {OAuth2Client} = require('google-auth-library');
 const app = express();
+// const CLIENT_ID = '867685073133-cc7g3jp9762653scgf951s3qcnai64ac.apps.googleusercontent.com'
+const CLIENT_ID = "CLIENT_ID";
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 4000;
+app.use(
+  cors(),
+  bodyParser.urlencoded({ extended: true}),
+  bodyParser.json()
+)
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.post('/', (req, res) => {
+  console.log(req.body.id_token);
+  const token = req.body.id_token
+  const client = new OAuth2Client(CLIENT_ID);
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    console.log(payload)
+  }
+  // Error: Invalid token signature
+  // Error: Wrong recipient, payload audience != requiredAudience
+  verify().catch(console.error);
 
-app.get('/', (req, res) => {
+  res.status(200).send('Welcome to GOogle API');
+});
+
+app.get('/google', (req, res) => {
   res.send('Welcome to GOogle API');
 });
 
